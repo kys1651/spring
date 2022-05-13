2492147196ba720d019f8e41b684057d950a5521
kimyongsu <akys159357@naver.com>
2022-05-13 23:19:47 +0900

    update 2022-05-13 11:19

--- src/main/java/hello/hellospring/controller/helloController.java
+++ src/main/java/hello/hellospring/controller/helloController.java
@@ -5,11 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class helloController {
+public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model){
-        model.addAttribute("data","hello!!");
+        model.addAttribute("data","hello!!!!");
         return "hello";
     }
 }
